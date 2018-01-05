a04b6283aaf531bdafdcd9e4bd854a253cc30af9
Pushkar Shembekar <pshembekar@ostglobal.com>
2018-01-04 23:31:39 -0500

    Second exercise

---
+++ Exercise2/ex2.py
@@ -0,0 +1,8 @@
+# A comment, this is so that you can read your program later
+print("I could have code like this")
+
+
+# You can also comment a code to disable it from running
+#print("This won't run")
+
+print ("This will run")
