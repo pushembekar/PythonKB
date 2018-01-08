770e66f31771fd5853fc11ddb3ea792d073d411a
Pushkar Shembekar <pshembekar@ostglobal.com>
2018-01-07 22:56:34 -0500

    Corrections

---
+++ Exercise5/ex5.py
@@ -0,0 +1,17 @@
+my_name = "Pushkar"
+my_age = 36
+my_eyes = "Black"
+my_hair = "Black"
+my_height = 70 #inches
+my_weight = 210 #pounds
+my_teeth = 'White'
+
+print(f"Let's talk about {my_name}")
+print(f"His height is {my_height} inches")
+print(f"He is {my_weight} heavy")
+print("Actually that is not very heavy, I think!")
+print(f"He's got {my_eyes} color eyes and {my_hair} color hair")
+print(f"He has {my_teeth} color teeth")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight}, I get {total}")
